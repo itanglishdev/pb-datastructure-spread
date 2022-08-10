6cccf4c752669b18e116bc55e7ac9b359c54a895
chris <chrishethertonweb@gmail.com>
2022-08-10 15:49:59 +0200

    Ex 1 done

---
+++ index.js
@@ -0,0 +1,25 @@
+const euroCountries = [
+  "italy", "spain", "georgia", "ukraine",
+]
+
+const asiaCountries = [
+  "thailand", "china", "mongolia", "singapore"
+]
+
+for (let i = 0; i < asiaCountries.length; i++) {
+  euroCountries.push(asiaCountries[i])
+
+}
+
+console.log(euroCountries);
+
+const euroCountriesTwo = [
+  "italy", "spain", "georgia", "ukraine",
+]
+
+const asiaCountriesTwo = [
+  "thailand", "china", "mongolia", "singapore"
+]
+
+const allCountriesTwo = euroCountriesTwo.concat(asiaCountriesTwo)
+console.log(allCountriesTwo);
